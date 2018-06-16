f89f8928ce96ae1161c071afed8254d8cb4baa66
Madan <madan_limbu@hotmail.co.uk>
2018-06-17 00:22:05 +0100

    base structure to capture image and store in app data directory

---
+++ app/src/main/java/com/example/madan/detector/CameraPreview.java
@@ -0,0 +1,76 @@
+package com.example.madan.detector;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        mCamera = camera;
+
+        //get notified when surface is created and destroyed
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        //after surface created, tell camera to draw preview
+        try{
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        }catch (IOException e){
+            Log.d(TAG, "Error setting camera preview: "+e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (mHolder.getSurface() == null){
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e){
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+        // start preview with new settings
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+
+        } catch (Exception e){
+            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
+}
--- app/src/main/java/com/example/madan/detector/MainActivity.java
+++ app/src/main/java/com/example/madan/detector/MainActivity.java
@@ -1,15 +1,214 @@
 package com.example.madan.detector;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "DetectorAPP";
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private Button captureButton;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+    public final int MY_PERMISSIONS_REQUEST_READ_CONTACTS= 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.camera_preview);
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+            // instance of Camera
+            mCamera = getCameraInstance();
+            if(mCamera == null){
+                Log.d(TAG, "mCamera is null");
+            }
+            //create Preview view and set it as content of activity
+            mPreview = new CameraPreview(this, mCamera);
+            if(mPreview == null){
+                Log.d(TAG, "mPreview is null");
+            }
+
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            if(preview == null){
+                Log.d(TAG, "preview is null");
+            }
+            preview.addView(mPreview);
+
+            setButtonListener();
+        }
+
+
     }
 
+    //create file Uri for saving image/video
+    private Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    //create file for saving image/video
+    private File getOutputMediaFile(int type){
+       // File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+      //          Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        File mediaStorageDir = null;
+        try {
+          mediaStorageDir = new File(String.valueOf(getExternalFilesDir(Environment.DIRECTORY_PICTURES)));
+        }catch (Exception e){
+            Log.d(TAG, e.getMessage());
+        }
+             // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        //finally create media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if(type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath()+File.separator+
+            "IMG_"+timeStamp+".jpg");
+        }else if(type == MEDIA_TYPE_VIDEO){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+            "VID_"+timeStamp+".mp4");
+        }else{
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if(pictureFile == null){
+                Log.d(TAG, "Error creating media file ");
+                return;
+            }
+            try{
+                System.out.println(pictureFile.toString());
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+
+            }catch (FileNotFoundException e){
+                Log.d(TAG, "File not found");
+            }catch (IOException e){
+                Log.d(TAG, "Error accessing file");
+            }
+        }
+    };
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try{
+            c = Camera.open();
+        }catch(Exception e){
+            Log.d(TAG, "camera instance not created");
+        }
+        return c;
+    }
+
+    public void setButtonListener(){
+        captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        //get an image from camera
+                        mCamera.takePicture(null, null, mPicture);
+                    }
+                }
+        );
+    }
+
+    //Camera shared resouce should be freeed on pause or close for other or same app to reuse camera in future
+    @Override
+    protected void onPause(){
+        super.onPause();
+     /*   if (mMediaRecorder != null) {
+            mMediaRecorder.reset();   // clear recorder configuration
+            mMediaRecorder.release(); // release the recorder object
+            mMediaRecorder = null;
+            mCamera.lock();           // lock camera for later use
+        }*/
+        if (mCamera != null){
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
 
 }
